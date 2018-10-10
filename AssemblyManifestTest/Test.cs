dae6ba1498c38daa7327f876ff606aae5e944dd7
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-10 15:16:43 +0800

    Initial commit

---
+++ AssemblyManifestTest/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Buffers;
+
+namespace AssemblyManifestTest
+{
+    class Test
+    {
+        public void DoSomething()
+        {
+            byte[] test = ArrayPool<byte>.Shared.Rent(10);
+        }
+    }
+}
